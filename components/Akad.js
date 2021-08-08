00c56290cb86182d95ff928aed4d9670399b9171
Fahmi Alfareza <fahmialfareza@icloud.com>
2021-08-09 06:49:32 +0700

    margin update

--- components/Akad.js
+++ components/Akad.js
@@ -88,7 +88,7 @@ function Akad() {
         </div>
       </section>
 
-      <section className="mt-5 mb-7">
+      <section className="mt-5 mb-5">
         <div className="container">
           <div className="row ">
             <div className="pesan-rspv mb-5 text-center mr-3">
