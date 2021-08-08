32d1c2cbe0ec0949c038d756c92af5a259a21c1e
santoadji21 <ahmad.aji.santoso21@gmail.com>
2021-08-09 06:06:30 +0700

    add responsive

--- public/js/script.js
+++ public/js/script.js
@@ -1,9 +1,9 @@
 AOS.init({
   // Global settings:
   disable: false, // accepts following values: 'phone', 'tablet', 'mobile', boolean, expression or function
-  startEvent: 'DOMContentLoaded', // name of the event dispatched on the document, that AOS should initialize on
-  initClassName: 'aos-init', // class applied after initialization
-  animatedClassName: 'aos-animate', // class applied on animation
+  startEvent: "DOMContentLoaded", // name of the event dispatched on the document, that AOS should initialize on
+  initClassName: "aos-init", // class applied after initialization
+  animatedClassName: "aos-animate", // class applied on animation
   useClassNames: false, // if true, will add content of `data-aos` as classes on scroll
   disableMutationObserver: false, // disables automatic mutations' detections (advanced)
   debounceDelay: 50, // the delay on debounce used while resizing window (advanced)
@@ -13,37 +13,64 @@ AOS.init({
   offset: 120, // offset (in px) from the original trigger point
   delay: 2, // values from 0 to 3000, with step 50ms
   duration: 400, // values from 0 to 3000, with step 50ms
-  easing: 'ease', // default easing for AOS animations
+  easing: "ease", // default easing for AOS animations
   once: false, // whether animation should happen only once - while scrolling down
   mirror: false, // whether elements should animate out while scrolling past them
-  anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
+  anchorPlacement: "top-bottom", // defines which position of the element regarding to window should trigger the animation
 });
 
 $(document).ready(function () {
   if (
-    window.matchMedia('(max-width: 767px)').matches ||
-    window.matchMedia('(max-width: 640px)').matches ||
-    window.matchMedia('(max-width: 667px)').matches ||
-    window.matchMedia('(max-width: 800px)').matches
+    window.matchMedia("(max-width: 767px)").matches ||
+    window.matchMedia("(max-width: 640px)").matches ||
+    window.matchMedia("(max-width: 667px)").matches ||
+    window.matchMedia("(max-width: 800px)").matches
   ) {
-    $('#navbar').removeClass('fixed-top mb-4 mt-4');
-    $('#navbar').addClass('fixed-bottom');
-    $('#navbar-list').removeClass('mx-auto');
-    $('#navbar-list').removeClass('mb-2');
-    $('#navbar-list').removeClass('mt-2');
-    $('#btn-ami').removeClass('ml-4');
-    $('#btn-ami').addClass('ml-1');
-    $('#btn-huda').removeClass('ml-4');
-    $('#btn-huda').addClass('ml-1 mr-1');
-    $('#rsvp').removeClass('d-flex');
-    $('.pesan-rspv').removeClass('col mr-3');
-    $('.pesan-rspv').addClass('col-sm-4');
-    $('.waktu').removeClass('col');
-    $('.waktu').addClass('col-sm-4 text-center');
-    $('.chat-ornament-top').removeClass('mr-2');
-    $('.chat-ornament-bottom').removeClass('mr-2');
-    $('.chat').removeClass('mr-7');
-    $('.chat').addClass('mr-2');
+    $("#navbar").removeClass("fixed-top mb-4 mt-4");
+    $("#navbar").addClass("fixed-bottom");
+    $("#navbar-list").removeClass("mx-auto");
+    $("#navbar-list").removeClass("mb-2");
+    $("#navbar-list").removeClass("mt-2");
+    $("#btn-ami").removeClass("ml-4");
+    $("#btn-ami").addClass("ml-1");
+    $("#btn-huda").removeClass("ml-4");
+    $("#btn-huda").addClass("ml-1 mr-1");
+    $("#rsvp").removeClass("d-flex");
+    $(".pesan-rspv").removeClass("col mr-3");
+    $(".pesan-rspv").addClass("col-sm-4");
+    $(".waktu").removeClass("col");
+    $(".waktu").addClass("col-sm-4 text-center");
+    $(".chat-ornament-top").removeClass("mr-2");
+    $(".chat-ornament-bottom").removeClass("mr-2");
+    $(".chat").removeClass("mr-7");
+    $(".chat").addClass("mr-2");
+    // $(".akad").removeClass("text-left");
+  }
+
+  if (
+    window.matchMedia("(min-width: 767px)").matches ||
+    window.matchMedia("(min-width: 640px)").matches ||
+    window.matchMedia("(min-width: 667px)").matches ||
+    window.matchMedia("(min-width: 400px)").matches
+  ) {
+    $("#navbar").removeClass("fixed-top mb-4 mt-4");
+    $("#navbar").addClass("fixed-bottom");
+    $("#navbar-list").removeClass("mx-auto");
+    $("#navbar-list").removeClass("mb-2");
+    $("#navbar-list").removeClass("mt-2");
+    $("#btn-ami").removeClass("ml-4");
+    $("#btn-ami").addClass("ml-1");
+    $("#btn-huda").removeClass("ml-4");
+    $("#btn-huda").addClass("ml-1 mr-1");
+    $("#rsvp").removeClass("d-flex");
+    $(".pesan-rspv").removeClass("col mr-3");
+    $(".pesan-rspv").addClass("col-sm-4");
+    $(".waktu").removeClass("col");
+    $(".waktu").addClass("col-sm-4 text-center");
+    $(".chat-ornament-top").removeClass("mr-2");
+    $(".chat-ornament-bottom").removeClass("mr-2");
+    $(".chat").removeClass("mr-7");
+    $(".chat").addClass("mr-2");
     // $(".akad").removeClass("text-left");
   }
 
