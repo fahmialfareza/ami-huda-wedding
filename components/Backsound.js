810607a4f04615ed67e99a1d7c022c111a3f6cfc
Fahmi Alfareza <fahmialfareza@icloud.com>
2021-08-11 15:12:20 +0700

    Audio fixed

--- components/Backsound.js
+++ components/Backsound.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Backsound() {
   return (
     <>
-      <audio allow="autoplay" autoPlay="true" hidden="hidden" loop="true">
+      <audio id="nasheed" hidden="hidden" loop="true">
         <source src="/audio/theweddingnasheed.mp3" />
       </audio>
     </>
---
+++ public/js/audio.js
@@ -0,0 +1,6 @@
+var audio = document.getElementById('nasheed');
+
+window.addEventListener('load', () => {
+  audio.loop = true;
+  audio.play();
+});
