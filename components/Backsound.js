eb713e52af5493ab4ff026d9094ec3236e8db148
Fahmi Alfareza <fahmialfareza@icloud.com>
2021-08-12 07:08:14 +0700

    audio play

--- components/Backsound.js
+++ components/Backsound.js
@@ -1,9 +1,15 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 function Backsound() {
+  useEffect(() => {
+    const audio = document.getElementById('audioNasheed');
+
+    audio.play();
+  }, []);
+
   return (
     <>
-      <audio loop autoPlay>
+      <audio allow="autoplay" loop autoPlay id="audioNasheed">
         <source src="/audio/theweddingnasheed.mp3" />
       </audio>
     </>
